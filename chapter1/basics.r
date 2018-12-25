b45059b39b92222ba733cfac55b63fbee6f76f31
Kasidis Satangmongkol <eskimo.toy@gmail.com>
2018-12-25 07:31:39 +0700

    Create basics.r

---
+++ chapter1/basics.r
@@ -0,0 +1,32 @@
+# Getting Started with R
+# https://datarockie.com
+
+# our facebook page
+# https://www.facebook.com/datarockie
+
+# example code for EP3-EP4 
+
+# R as a calculator
+1 + 1
+4 - 2
+2 * 2
+4 / 2
+2 ** 3
+5 %% 2
+
+# create variables
+x <- 200
+y <- 300
+print(x + y)
+
+# another example of variable names
+income_2018 <- 50000
+pct_increase <- 0.20
+income_2019 <- income_2018 * (1+pct_increase)
+
+# vector
+x <- c(100, 200, 300, 400, 500)
+y <- c(5, 10, 15, 20, 25)
+x * 3
+x + x
+x + y
