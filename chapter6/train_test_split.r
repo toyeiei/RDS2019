b6ac3e35dce7489e1d779adc3d1c6066953b8db5
Kasidis Satangmongkol <eskimo.toy@gmail.com>
2018-12-25 14:40:43 +0700

    Update train_test_split.r

--- chapter6/train_test_split.r
+++ chapter6/train_test_split.r
@@ -6,13 +6,22 @@
 
 # example code for split data into train / test sets
 # assume we have a dataframe called 'df'
+# training 80%, testing 20%
+
+df <- mtcars
 
 set.seed(99)
 idx <- sample(nrow(df), 0.8*nrow(df))
 train_df <- df[idx, ]
 test_df <- df[-idx, ]
 
-nrow(train_df)
-nrow(test_df)
+# build model with train_df
+lm_mod <- lm(mpg ~ ., data = train_df)
+
+# predict test_df
+p <- predict(lm_mod, newdata = test_df)
+
+# compute rmse
+rmse <- sqrt(mean((p - test_df$mpg)**2))
 
 # DONE!
