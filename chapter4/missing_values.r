8e9fc531e467ec09f993ce96b2ec55b24231d23a
Kasidis Satangmongkol <eskimo.toy@gmail.com>
2018-12-25 11:46:13 +0700

    Update missing_values.r

--- chapter4/missing_values.r
+++ chapter4/missing_values.r
@@ -9,6 +9,8 @@
 # missing value in R shown as NA
 x <- c(100, 200, 300, 400, 500, NA)
 is.na(x) # FALSE FALSE FALSE FALSE FALSE TRUE
+mean(x) # this will not work because there is NA (unknown value)
+mean(x, na.rm = TRUE)
 
 # remove NA from vector x
 x[!is.na(x)]
@@ -29,6 +31,12 @@ clean_df <- test_df[complete.cases(test_df), ]
 # 2. replace NA - statistician calls this technique "imputation"
 # replace NA with 100
 test_df$scores[is.na(test_df$scores)] <- 100
+
+# replace NA with mean 
+test_df$scores[is.na(test_df$scores)] <- mean(test_df$scores, na.rm = TRUE)
+
+# replace NA with median
+test_df$scores[is.na(test_df$scores)] <- median(test_df$scores, na.rm = TRUE)
 
 # export dataframe as csv file
 # use write.csv() function to export df
